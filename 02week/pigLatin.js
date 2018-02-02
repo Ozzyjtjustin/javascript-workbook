c0a7a8751e37187e0ee07c5ff077e19993f91933
Justin Oswald <justinoswald@Users-MacBook-Pro.local>
2018-02-02 11:41:52 -0600

    Pig latin game now passing all tests

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -7,32 +7,28 @@ const rl = readline.createInterface({
  output: process.stdout
 });
 
-const pigLatin = (input) => {      //create a function named pigLatinTraslator
+const pigLatin = (input) => {      //create a function named pigLatin that takes in one argument
   const vowelArray = ["a","e","i","o","u"]  //create an array of Vowels
-  input = input.trim().toLowerCase()        //format the input to remove white space and convert to lowercase
-  if(typeof input === typeof "hello"){      //find out if the input is a string
+  if(typeof input === typeof "String"){      //check if the input is valid by finding out if the input is a string
+    input = input.trim().toLowerCase()      //format the input to remove white space and convert to lowercase
     if(vowelArray.includes(input[0])){      //find out if the first letter in the input is a vowel
-     return input + "yay"                   //if so return a new string adding "yay"
-   }else{                                   //find the first instance of a vowel
-       let vowelObj = {}                    //create an empty vowel object
-       for (let i=0;i<vowelArray.length;i++){                     //iterate throgh the vowel array and
-           vowelObj[vowelArray[i]] = input.search(vowelArray[i])  //fill the vowel object with 5 properties and 5 valuse (indexes of each one's vowel)
-       }
-       let lowestNumber = 45          //create a variable containing the number of characters in the longest word possible
-       let splitLetter = ""           //create a variable for the letter of the first vowel
-       for (let key in vowelObj){     //iterate through the keys in the vowel object
-         if ((vowelObj[key] < lowestNumber) && (vowelObj[key] > 0)){  //see which index is the lowest while not being less that 1
-           lowestNumber = vowelObj[key]  //reasign lowestNumber variable with the lowest index
-           splitLetter = key             //reasign splitLetter variable with the key at the lowest index
-         }
-     }                                //using .split at the identified first vowel concat a new string containing the first vowel,
-                                      //the second half of the split input, the first half of the first input and finaly "ay"
-     let finalAnswer = splitLetter + input.split(splitLetter)[1] + input.split(splitLetter)[0] + "ay"
-     return finalAnswer
+      return input + "yay"                   //if so return a new string adding "yay"
+    }else{
+       let lowestVowelIndex = 45
+       let splitLetter                               //find the first instance of a vowel
+       vowelArray.forEach((vowel)=> {
+         let vowelIndex = input.indexOf(vowel)
+         if((vowelIndex > -1) && (vowelIndex < lowestVowelIndex)){
+           lowestVowelIndex = vowelIndex
+           splitLetter = input[lowestVowelIndex]
+          }
+       })
+       return console.log(input.substr(lowestVowelIndex) + input.split(splitLetter)[0] + "ay")
    }
- }else {console.log('please enter a word to be translated')}
-}
+  }else {console.log('please enter a word to be translated')}
+ }
 
+pigLatin("create")
 
 
 function getPrompt() {
