095d979ec3ed6ebb115f12836895ea17a16f875b
Justin Oswald <justinoswald@Users-MacBook-Pro.local>
2018-02-06 19:04:04 -0600

    first draft olymics

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const printListOfOlympians = () => {
+  let olympicsArray=[array]
+    olympicsArray.forEach((arrayItem) =>{
+      console.log(arrayItem)
+    })
+}
+printListOfOlympians("Shawn Wight","Gus Kinworth","Simon Biles","Shelby Inloes","Justin Oswald")
