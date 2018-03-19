aa74b3720dac9589bf7887e06a367e5a225eab48
Justin Oswald <justinoswald95@gmail.com>
2018-03-19 10:38:04 -0500

    Separating Container Components From Presentational Components

---
+++ 07week/react-tictactoe/src/Components/ContainerComponent.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import '../App.css';
+import {TicTacToePresenter} from './PresentationalComponent'
+
+class TicTacToeContainer extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      player:"X",
+      clickCount: 0,
+      xWins: null,
+      oWins: null,
+      board: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+      ]
+    }
+  };
+
+  clickTurnHandler=(row, column)=>{
+    const newPlayer = this.state.player;
+    const newClickCount = this.state.clickCount;
+    const newBoard = this.state.board;
+    const clickedRow = this.state.board[row];
+    clickedRow.splice(column,1, this.state.player);
+    newBoard.splice(row,1,clickedRow);
+    this.setState({clickCount: newClickCount + 1, board:newBoard, player: newPlayer === 'X' ? 'O' : 'X'});
+    this.checkForWin()
+    console.log(this.state.clickCount)
+  };
+
+  clickResetBoardHandler=()=>{
+    const resetPlayer = "X"
+    const resetClickCount = 0
+    const resetBoard = [[null, null, null],[null, null, null],[null, null, null]]
+    this.setState({player: resetPlayer, clickCount: resetClickCount, board: resetBoard})
+  };
+
+  clickResetWinCountHandler=()=>{
+    const resetWinCountX = 0
+    const resetWinCountO = 0
+    this.setState({xWins: resetWinCountX, oWins: resetWinCountO})
+  };
+          // checkForHorizontalWin=()=>{
+          //   this.state.board.forEach((arr, index)=>{
+          //     arr.every((box)=>{
+          //       if(box === this.state.player){
+          //         console.log('horizontal')
+          //         return true
+          //       }
+          //     })
+          //   })
+          // };
+  checkForHorizontalWin=()=>{
+    if((this.state.board[0][0] === this.state.player && this.state.board[0][1] === this.state.player && this.state.board[0][2] === this.state.player) ||
+     (this.state.board[1][0] === this.state.player && this.state.board[1][2] === this.state.player && this.state.board[1][2] === this.state.player) ||
+     (this.state.board[2][0] === this.state.player && this.state.board[2][1] === this.state.player && this.state.board[2][2] === this.state.player)){
+      return  true
+    }
+  };
+
+  checkForVerticalWin=()=>{
+    if((this.state.board[0][0] === this.state.player && this.state.board[1][0] === this.state.player && this.state.board[2][0] === this.state.player) ||
+     (this.state.board[0][1] === this.state.player && this.state.board[1][1] === this.state.player && this.state.board[2][1] === this.state.player) ||
+     (this.state.board[0][2] === this.state.player && this.state.board[1][2] === this.state.player && this.state.board[2][2] === this.state.player)){
+      return  true
+    }
+  };
+
+  checkForDiagonalWin=()=>{
+    if((this.state.board[0][0] === this.state.player && this.state.board[1][1] === this.state.player && this.state.board[2][2] === this.state.player) ||
+      (this.state.board[2][0] === this.state.player && this.state.board[1][1] === this.state.player && this.state.board[0][2] === this.state.player)){
+      return  true
+    }
+  };
+
+  checkForWin=()=>{
+    if(this.state.clickCount >= 4){
+      if(this.checkForHorizontalWin() || this.checkForVerticalWin() ||  this.checkForDiagonalWin()){
+        alert(this.state.player + " wins!")
+        if(this.state.player === "X"){
+          const newXWinCount = this.state.xWins
+          this.setState({xWins: newXWinCount + 1})
+        }else{
+          const newOWinCount = this.state.oWins
+          this.setState({oWins: newOWinCount + 1})
+        }
+      }
+    }
+  };
+
+
+
+  render(){
+    return <TicTacToePresenter
+    board={this.state.board}
+    xWinCount={this.state.xWins}
+    oWinCount={this.state.oWins}/>
+  }
+}
+
+export default TicTacToeContainer;
---
+++ 07week/react-tictactoe/src/styles.js
@@ -0,0 +1,55 @@
+const rowStyle = {
+  height: '110',
+  fontSize: '100px',
+  margin: '15px auto',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center'
+}
+
+const boxStyle = {
+  height: '110px',
+  width: '110px',
+  backgroundColor: 'lightblue',
+  margin: 'auto 20px',
+  border: '5px solid black',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center'
+}
+
+const buttonStyle ={
+  height: '50px',
+  width: '100px',
+  backgroundColor: 'grey',
+  border: '2.5px solid black',
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center'
+}
+
+const headerStyle = {
+  fontSize: '75px',
+  backgroundColor: "grey",
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center'
+}
+const footerStyle = {
+  height:'100px',
+  fontSize: '50px',
+  backgroundColor: "grey",
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center'
+}
+const background = 'pink url("http://www.clker.com/cliparts/6/8/Z/b/2/1/tic-tac-toe-md.png") fixed';
+
+export const styles = {
+  rowStyle: rowStyle,
+  boxStyle: boxStyle,
+  buttonStyle: buttonStyle,
+  headerStyle: headerStyle,
+  footerStyle: footerStyle,
+  urlStyle: background
+}
